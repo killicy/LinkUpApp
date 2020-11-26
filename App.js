273cee3bfc5b06f4476056d2f86cd5eb94a71514
RomanReyes1994 <73444927+RomanReyes1994@users.noreply.github.com>
2020-11-25 19:18:18 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Image, Alert, SafeAreaView, Button, Platform, Dimensions } from 'react-native';
+import MainPage from './app/screens/MainPage';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+
+export default function App() {
+
+
+  return <MainPage />;
+}
+
